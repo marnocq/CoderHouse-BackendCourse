89e0ba404f4b368be1a9056f7d4a3a0cabb14020
marnocq <martin.nocquet@icloud.com>
2021-07-09 20:57:12 -0300

    Correcciones

--- 06/archivo.js
+++ 06/archivo.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
+const path = require('path');
 
 class Archivo{
     constructor(nombre){
@@ -9,20 +10,26 @@ class Archivo{
    async leer(){           
         try{
             const contenido = await fs.promises.readFile(this.nombre, 'utf-8')            
-            console.log(JSON.parse(contenido))  
+            console.log(JSON.parse(contenido)) 
         }
         catch(err){
-            console.log('[]', err)
+            console.log('No hay datos en el archivo []', err)
         }                
     }
 
    async guardar(producto){        
         try{
+            const contenido = await fs.promises.readFile(this.nombre, 'utf-8')
+            if(contenido){                                                              
+                this.productos.push(JSON.parse(contenido))                
+                this.productos = this.productos.flat()               
+            }            
             const produ = JSON.parse(producto)
             produ.id = this.productos.length +1             
             this.productos.push(produ)
-            console.log("esto es del console log"+this.productos)
+            
             await fs.promises.writeFile(this.nombre, JSON.stringify(this.productos))
+            
             console.log('agregado con exito!')
         }
         catch(err){
@@ -30,24 +37,26 @@ class Archivo{
         }
     }
 
-    borrar(){
-        fs.unlink(this.nombre, error => {
-            if(error){
-                console.log('Ocurrio un error al intentar borrar el archivo')
-            }else{
-                console.log('Se borro el archivo!')
-            }
-        })
-
+    async borrar(){
+        try{
+            await fs.promises.unlink(this.nombre, error => {
+                if (error) {
+                    console.log('Ocurrio un error al intentar borrar el archivo')
+                } else {
+                    console.log('Se borro el archivo!')
+                }
+            })
+        }catch(err){
+            console.log('Ocurrio un error al intentar borrar el archivo')
+        }    
     }
     
 }
+const dirPath = path.join(__dirname, '/productos.txt');
+let ar = new Archivo(`${dirPath}`)
 
-let ar = new Archivo("/Users/noc/CODERHOUSE/Code/06/productos.txt")
-
-// ar.leer()
-ar.guardar('{"title":"Escuadra", "price":123.25, "thumbnail":"asdasdasd"}')
-ar.guardar('{"title":"Lapiz", "price":3.25, "thumbnail":"aaaaaaa"}')
-ar.guardar('{"title":"Cuaderno", "price":58.5, "thumbnail":"bbbbbbbb"}')
+//ar.guardar('{"title":"Escuadra", "price":123.25, "thumbnail":"asdasdasd"}')
+//ar.guardar('{"title":"Lapiz", "price":3.25, "thumbnail":"aaaaaaa"}')
+//ar.guardar('{"title":"Cuaderno", "price":58.5, "thumbnail":"bbbbbbbb"}')
 ar.leer()
 //ar.borrar()
